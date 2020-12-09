92066d70c65a099ea01957ecb6ac4710d4866e93
Fung Yeung <yeung.kei.fung@gmail.com>
2020-12-09 21:19:15 +0800

    finish code part of 2.62

--- Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.62.scm
+++ Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.62.scm
@@ -5,23 +5,15 @@
 ; an else clause has been add since we need to cater for when x1 != x2,
 ; which is where both value need to be taken from both set
 
-(define (union-set set1 set2)
-  (if (or (null? set1) (null? set2))
-      '()
-      (let ((x1 (car set1)) (x2 (car set2)))
-        (cond ((= x1 x2)
-               (cons x1
-                     (union-set (cdr set1)
-                                       (cdr set2))))
-              ((< x1 x2)
-               (union-set (cdr set1) set2))
-              ((< x2 x1)
-               (union-set set1 (cdr set2)))
-              (else (cons 
-                      (cons x1 x2) 
-                      (union-set 
-                        (cdr set1) 
-                        (cdr set2)))
-        ))))
-)
+ (define (union-set set1 set2) 
+   (cond  ((null? set1) set2) 
+          ((null? set2) set1) 
+          ((= (car set1) (car set2))  
+           (cons (car set1) (union-set (cdr set1) (cdr set2)))) 
+          ((< (car set1) (car set2))   
+           (cons (car set1) (union-set (cdr set1) set2))) 
+          (else  
+           (cons (car set2) (union-set set1 (cdr set2)))))) 
+  
+
 
