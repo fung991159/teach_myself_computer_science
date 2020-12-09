20cdee8fe789ea5d2a73bdda04b9a316a4fef48f
Fung Yeung <yeung.kei.fung@gmail.com>
2020-12-09 09:20:19 +0800

    finish code part of 2.61

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.61.scm
@@ -0,0 +1,16 @@
+; Exercise 2.61. Give an implementation of adjoin-set using the ordered representation. By analogy
+; with element-of-set? show how to take advantage of the ordering to produce a procedure that
+; requires on the average about half as many steps as with the unordered representation.
+
+; >> so far so good
+
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= (car set) x) set)
+        ((> (car set) x) (cons x set))
+        ((< (car set) x) (cons (car set) (adjoin-set x (cdr set)))
+  )
+))
+
+
