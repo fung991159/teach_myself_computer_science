7089f155cd4e93fc7851550a7cc8b147b10f4f84
Fung Yeung <yeung.kei.fung@gmail.com>
2020-12-09 13:16:19 +0800

    finish code part of 2.61

--- Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.61.scm
+++ Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.61.scm
@@ -2,7 +2,10 @@
 ; with element-of-set? show how to take advantage of the ordering to produce a procedure that
 ; requires on the average about half as many steps as with the unordered representation.
 
-; >> so far so good, adjoin-set will stop 
+; >> so far so good, new adjoin-set will 
+; >> stop when x is equal to any element of set or it is end of the set
+; >> hence it is possible to have n/2 factor speed gain just as element-of-set?
+
 (define (adjoin-set x set)
   (cond ((null? set) (list x))
         ((= (car set) x) set)
