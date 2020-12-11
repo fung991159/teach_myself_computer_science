894b4bc2a85197b4b2e07aa73d480b53bf7b90d1
Fung Yeung <yeung.kei.fung@gmail.com>
2020-12-11 08:20:31 +0800

    finish code part of 2.63

--- Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.63.scm
+++ Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.63.scm
@@ -15,10 +15,9 @@
                               (right-branch tree)
                               result-list)))))
   (copy-to-list tree '()))
-; Do the two procedures produce the same results for every tree? If not, 
+
+; a. Do the two procedures produce the same results for every tree? If not, 
 ; how do the results differ? What lists do the two procedures produce fot the trees in Figure 2.16?
-
-
 (define tree1 '(7 
                 (3 
                   (1 () ()) 
@@ -44,3 +43,8 @@
                   (11 () ()))))
 
 ; all return the same result (1 3 5 7 9 11)
+
+;b. Do the two procedures have the same order of growth in the number of steps required to convert a
+; balanced tree with n elements to a list? If not, which one grows more slowly?
+;>>  recusive one would use more memory to store the state, and require more steps
+;>> to sort out the previous stored state once it reach the "bottom"
