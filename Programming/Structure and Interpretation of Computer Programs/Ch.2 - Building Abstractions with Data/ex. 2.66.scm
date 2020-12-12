fcd017efe6433cae901fd5c6ec16c2bedacb9e85
Fung Yeung <yeung.kei.fung@gmail.com>
2020-12-12 09:58:54 +0800

    complted 2.66

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.2 - Building Abstractions with Data/ex. 2.66.scm
@@ -0,0 +1,12 @@
+;Exercise 2.66: Implement the lookup procedure for the case where the set of records 
+;is structured as a binary tree, ordered by the numerical values of the keys.
+
+; this is just rewriting element-of-set for ordered list, + converting the input from 
+; tree to list first
+
+(define (lookup given-key set-of-records)
+  (let (list-of-records (tree->list2 set-of-records))
+  (cond ((null? list-of-records) false)
+        ((= given-key (car list-of-records)) true)
+        ((< given-key (car list-of-records)) false)
+        (else (lookup given-key (cdr list-of-records))))))
