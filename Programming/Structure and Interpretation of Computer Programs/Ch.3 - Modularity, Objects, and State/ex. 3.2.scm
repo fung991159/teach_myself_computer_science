3de9d60710a052b991f3b3ea3937eaed64385252
Fung Yeung <yeung.kei.fung@gmail.com>
2020-12-27 11:49:29 +0800

    attempted 3.2

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.2.scm
@@ -0,0 +1,32 @@
+Exercise 3.2. In software-testing applications, it is useful to be able to count the number of times a given
+procedure is called during the course of a computation. 
+Write a procedure make-monitored that takes
+as input a procedure, f, that itself takes one input.
+The result returned by make-monitored is a third
+procedure, say mf, that keeps track of the number of times it has been called by maintaining an internal
+counter. If the input to mf is the special symbol how-many-calls?, then mf returns the value of the
+counter. If the input is the special symbol reset-count, then mf resets the counter to zero. For any
+other input, mf returns the result of calling f on that input and increments the counter. For instance, we
+could make a monitored version of the sqrt procedure:
+(define s (make-monitored sqrt))
+(s 100)
+10
+(s 'how-many-calls?)
+1
+
+;; my attmpet to this question, somehow need to "display" the function call
+(define (make-monitored f)
+  (let ((cnt 0))
+    (lambda (x)
+      (if (symbol? x)
+        (cond ((eq? x 'how-many-calls) cnt)
+              ((eq? x 'reset-count) (begin (set! cnt 0) "reset count done"))
+              (else "unknown command"))
+        (begin (display (f x))
+               (set! cnt (+ cnt 1)))
+    )
+  )
+))
+
+(define s (make-monitored sqrt))
+(s 100)
