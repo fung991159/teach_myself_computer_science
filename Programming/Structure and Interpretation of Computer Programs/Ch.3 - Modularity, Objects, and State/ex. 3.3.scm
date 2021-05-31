b53d5b31b51fdcc3cdd55b9cce313078d40e5bfc
Fung Yeung <yeung.kei.fung@gmail.com>
2021-05-31 17:34:13 +0800

    add comment and edit formatting a bit

--- Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.3.scm
+++ Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.3.scm
@@ -1,14 +1,15 @@
-Exercise 3.3. Modify the make-account procedure so that it creates password-protected accounts.
-That is, make-account should take a symbol as an additional argument, as in
-(define acc (make-account 100 'secret-password))
-The resulting account object should process a request only if it is accompanied by the password with
-which the account was created, and should otherwise return a complaint:
-((acc 'secret-password 'withdraw) 40)
-60
-((acc 'some-other-password 'deposit) 50)
-"Incorrect password"
+; Exercise 3.3. Modify the make-account procedure so that it creates password-protected accounts.
+; That is, make-account should take a symbol as an additional argument, as in
+; (define acc (make-account 100 'secret-password))
+; The resulting account object should process a request only if it is accompanied by the password with
+; which the account was created, and should otherwise return a complaint:
+; ((acc 'secret-password 'withdraw) 40)
+; 60
+; ((acc 'some-other-password 'deposit) 50)
+; "Incorrect password"
 
-; >> before dispatching chedck if password is match, if doesn't match then no need proceed further
+>> basically just adding the password checking logic before disptaching job
+
 (define (make-account balance password)
   (define (withdraw amount)
     (if (>= balance amount)
@@ -27,3 +28,7 @@ which the account was created, and should otherwise return a complaint:
           (else (error "Unknown request -- MAKE-ACCOUNT"
                        m))))
   dispatch)
+
+(define acc (make-account 100 'test))
+(display ((acc 'test 'withdraw) 10))
+(newline)
