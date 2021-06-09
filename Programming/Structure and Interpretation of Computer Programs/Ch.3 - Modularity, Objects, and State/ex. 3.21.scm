4bff6337bb9c0e1f3e4ce88fa0d5edd23f5e91ea
Fung Yeung <yeung.kei.fung@gmail.com>
2021-06-09 17:40:15 +0800

    complete ex.3.21

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.21.scm
@@ -0,0 +1,36 @@
+Exercise 3.21: Ben Bitdiddle decides to test the queue implementation described
+above. He types in the procedures to the Lisp interpreter and proceeds to try
+them out:
+
+(define q1 (make-queue))
+(insert-queue! q1 'a)
+(insert-queue! q1 'b)
+(delete-queue! q1)
+(delete-queue! q1)
+
+((a) a)
+((a b) b)
+((b) b)
+(() b)
+
+``It's all wrong!'' he complains. ``The interpreter's response shows that the
+last item is inserted into the queue twice. And when I delete both items, the
+second b is still there, so the queue isn't empty, even though it's supposed to
+be.'' Eva Lu Ator suggests that Ben has misunderstood what is happening. ``It's
+not that the items are going into the queue twice,'' she explains. ``It's just
+that the standard Lisp printer doesn't know how to make sense of the queue
+representation. If you want to see the queue printed correctly, you'll have to
+define your own print procedure for queues.'' Explain what Eva Lu is talking
+about. In particular, show why Ben's examples produce the printed results that
+they do. Define a procedure print-queue that takes a queue as input and prints
+the sequence of items in the queue.
+
+>> basically we just start from queue's front pointer and print everythig from there, cater for null of course
+
+(define (print-q queue)
+    (define (get-queue q)
+      (cond ((not (null? q))
+              (cons (car q) (get-queue (cdr q)))
+      )
+    ))
+    (get-queue (front-ptr queue))
