02a671a2ac86d18514748e1f0e679a038435ec1f
Fung Yeung <yeung.kei.fung@gmail.com>
2021-06-24 07:25:28 +0800

    completed 3.39

--- Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.39.scm
+++ Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.39.scm
@@ -7,6 +7,9 @@ above remain if we instead serialize execution as follows:
 (parallel-execute (lambda () (set! x ((s (lambda () (* x x))))))
                   (s (lambda () (set! x (+ x 1)))))
 
->> since the first procedure is not serialized. It is possible to intervine
-with the 2nd procedure by setting x to 10*10 = 100.
-The other result are 101 (10*10 + 1) and 121 ((10+1)*11)
+>> since the first procedure is not entirely serialized.
+It is possible to intervine with the 2nd procedure by
+first accessing x=10, computing new x to 10*10 = 100, and overwrite
+the 2nd procedure result of x = 11. leaving us with final result x = 100.
+The other result are 101 (10*10 + 1) and 121 ((10+1)*11) if the procs happens
+to not intertwine by luck.
