1e07c59b334a28709e45bb924c583ec26f961cf8
Fung Yeung <yeung.kei.fung@gmail.com>
2021-07-01 10:34:39 +0800

    completed ex3.50

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.3 - Modularity, Objects, and State/ex. 3.50.scm
@@ -0,0 +1,15 @@
+Exercise 3.50. Complete the following
+definition, which generalizes
+stream-map to allow procedures
+that take multiple arguments, analogous
+to map in section 2.2.3
+
+>> just minor edition
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+    the-empty-stream
+    (cons-stream
+    (apply proc (map stream-car argstreams))
+    (apply stream-map
+          (cons proc (map stream-cdr argstreams))))))
