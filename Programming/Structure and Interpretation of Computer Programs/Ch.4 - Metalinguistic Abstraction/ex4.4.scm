0977aa3168783b1a58f3506273d9325214985427
Fung Yeung <yeung.kei.fung@gmail.com>
2021-08-04 07:46:44 +0800

    completed 4.4

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.4 - Metalinguistic Abstraction/ex4.4.scm
@@ -0,0 +1,30 @@
+Exercise 4.4. Recall the definitions of the special forms and and or from chapter 1:
+●and: The expressions are evaluated from left to right. If any expression evaluates to false, false is
+returned; any remaining expressions are not evaluated. If all the expressions evaluate to true
+values, the value of the last expression is returned. If there are no expressions then true is returned.
+●or: The expressions are evaluated from left to right. If any expression evaluates to a true value,
+that value is returned; any remaining expressions are not evaluated. If all expressions evaluate to
+false, or if there are no expressions, then false is returned.
+Install and and or as new special forms for the evaluator by defining appropriate syntax procedures and
+evaluation procedures eval-and and eval-or. Alternatively, show how to implement and and or as
+derived expressions.
+
+>> surprisingly the implementation of "and" and "or" is actually very similar
+
+>> special form
+((and? exp) (tagged-lists? exp 'and))
+(define (and-lst) (cdr exp))
+(define (first-predicate and-lst) (car and-lst))
+(define (rest-predicate and-lst) (cdr and-lst))
+(define (no-predicate? and-lst) (null? and-lst))
+
+(define (eval-and exps env)
+  (cond ((no-predicate? exps) #t))
+        ((not (true? (eval (first-predicate exps)))) #f)
+        (else (eval-and (rest-predicate exps) env)))
+
+((or? exp) (tagged-lists? exp 'or))
+(define (eval-or exps env)
+  (cond ((no-predicate? exps) #f))
+        ((true? (eval (first-predicate exps))) #t)
+        (else (eval-or (rest-predicate exps) env)))
