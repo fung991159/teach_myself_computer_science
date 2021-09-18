e703c7e56414a03a80b2f1b6a507e04f8c76b3dd
Fung Yeung <yeung.kei.fung@gmail.com>
2021-09-18 20:05:20 +0800

    completed 4.32

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.4 - Metalinguistic Abstraction/ex4.32.scm
@@ -0,0 +1,4 @@
+Exercise 4.32. Give some examples that illustrate the difference between the streams of chapter 3 and the
+``lazier'' lazy lists described in this section. How can you take advantage of this extra laziness?
+
+>> as the text say, the lazier version doesn't evaluate until it get called. This makes the order of variable irrelvant. Which can be handy, especially for case as "integral"
