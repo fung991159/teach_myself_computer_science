e1022eeda6860e125b10249a7feedfd8fc1cd1a2
Fung Yeung <yeung.kei.fung@gmail.com>
2021-09-24 20:21:48 +0800

    completed first section of 4.36

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.4 - Metalinguistic Abstraction/ex4.36.scm
@@ -0,0 +1,16 @@
+Exercise 4.36: Exercise 3.69 discussed how to generate the
+stream of all Pythagorean triples, with no upper bound on
+566
+the size of the integers to be searched. Explain why simply
+replacing an-integer-between by an-integer-starting-
+from in the procedure in Exercise 4.35 is not an adequate
+way to generate arbitrary Pythagorean triples.
+
+>> i, j, k will always move in sync, hence the procedure will never end as no solution could be found
+
+Write a pro-
+cedure that actually will accomplish this. (that is, write a
+procedure for which repeatedly typing try-again would in
+principle eventually generate all Pythagorean triples.)
+
+>> no clue
