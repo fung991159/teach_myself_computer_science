3b88c34beea05941b19fc8622f60ada04e324674
Fung Yeung <yeung.kei.fung@gmail.com>
2022-04-18 14:19:29 +0800

    tried ex5.20

---
+++ Programming/Structure and Interpretation of Computer Programs/Ch.5 - Comuting with Register Machine/ex5.20.scm
@@ -0,0 +1,11 @@
+Exercise 5.20: Draw the box-and-pointer representation
+and the memory-vector representation (as in Figure 5.14)
+of the list structure produced by
+  (define x (cons 1 2))
+  (define y (list x x))
+with the free pointer initially p1. What is the final value of
+free ? What pointers represent the values of x and y ?
+
+>> did my best attempt at ex5.20.png, however, not sure why index 0 never have any item, also where is the free pointer at the end?
+
+
