b4680b61ce048ac5222c2409d93f34fd756d8dd0
Fung Yeung <yeung.kei.fung@gmail.com>
2022-08-21 10:38:13 +0800

    Add Assebly code Mult

--- Computer Archiecture/nand2tetris/projects/04/mult/Mult.asm
+++ Computer Archiecture/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,46 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// define variables
+@i  // @16
+M=1
+
+@sum // @17
+M=0
+
+(LOOP)
+    // if (i > R1) goto @STOP
+    @i
+    D=M
+    @R1
+    D=D-M
+    @STOP
+    D;JGT
+
+
+    // sum = sum + RAM[0]
+    @R0
+    D=M
+    @sum
+    M=M+D
+
+    // i = i + 1
+    @i
+    M=M+1
+    D=M
+
+    // start looping until  condition met
+    @LOOP
+    0; JMP
+
+(STOP)
+    // R2 = sum
+    @sum
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0;JMP
