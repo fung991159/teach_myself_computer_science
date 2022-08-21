2627d4286494781aa242e5185be2cbb8c4837fdf
Fung Yeung <yeung.kei.fung@gmail.com>
2022-08-21 20:59:14 +0800

    Completed Fill.asm

--- Computer Archiecture/nand2tetris/projects/04/fill/Fill.asm
+++ Computer Archiecture/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAIN_LOOP)
+    //  if keyboard pressed (value not equal to zero), GOTO PRESSED
+    @KBD
+    D=M
+
+    @SCREEN_BLACK
+    D; JNE
+
+    // otherwise make screen white
+    @SCREEN_WHITE
+    0; JMP
+
+
+(SCREEN_BLACK)
+@i
+M=0
+
+(SCREEN_BLACK_LOOP)
+    // if i = 8192, goto END
+    @8192
+    D=A
+    @i
+    D=M-D
+
+    @SCREEN_STOP
+    D; JEQ
+
+    // *(@SCREEN + i) = -1
+    @16384
+    D=A
+    @i
+    A=D+M  // D: 16384/@SCREEN POINTER, M=i 
+    M=-1
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    // GOTO DRAW_SCREEN
+    @SCREEN_BLACK_LOOP
+    0; JMP
+
+(SCREEN_WHITE)
+@i
+M=0
+
+(SCREEN_WHITE_LOOP)
+    // if i = 8192, goto END
+    @8192
+    D=A
+    @i
+    D=M-D
+
+    @SCREEN_STOP
+    D; JEQ
+
+    // *(@SCREEN + i) = -1
+    @16384
+    D=A
+    @i
+    A=D+M
+    M=0
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    // GOTO DRAW_SCREEN
+    @SCREEN_WHITE_LOOP
+    0; JMP
+
+(SCREEN_STOP)
+    // after drawing black or white go back to main loop to listen for next keyboard event
+    @MAIN_LOOP
+    0; JMP
