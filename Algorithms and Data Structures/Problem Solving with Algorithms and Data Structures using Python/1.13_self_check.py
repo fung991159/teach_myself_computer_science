909a8ce6e83976996497bed40453c640deee65f3
Fung Yeung <yeung.kei.fung@gmail.com>
2024-11-20 08:59:32 +1100

    extended *-></

---
+++ Algorithms and Data Structures/Problem Solving with Algorithms and Data Structures using Python/1.13_self_check.py
@@ -0,0 +1,71 @@
+def gcd(m, n):
+    while m % n != 0:
+        m, n = n, m % n
+    return n
+
+
+def lcd(m, n):
+    return abs(m * n) // gcd(m, n)
+
+
+class Fraction:
+    def __init__(self, top, bottom):
+        self.num = top
+        self.den = bottom
+
+    def __str__(self):
+        return "{:d}/{:d}".format(self.num, self.den)
+
+    def __eq__(self, other_fraction):
+        first_num = self.num * other_fraction.den
+        second_num = other_fraction.num * self.den
+
+        return first_num == second_num
+
+    def __add__(self, other_fraction):
+        new_num = self.num * other_fraction.den + self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        cmmn = gcd(new_num, new_den)
+        return Fraction(new_num // cmmn, new_den // cmmn)
+
+    def show(self):
+        print("{:d}/{:d}".format(self.num, self.den))
+
+    def __mul__(self, other_fraction):
+        new_num = self.num * other_fraction.num
+        new_den = self.den * other_fraction.den
+        cmmn = gcd(new_num, new_den)
+        return Fraction(new_num // cmmn, new_den // cmmn)
+
+    def __truediv__(self, other_fraction):
+        new_num = self.num * other_fraction.den
+        new_den = self.den * other_fraction.num
+        cmmn = gcd(new_num, new_den)
+        return Fraction(new_num // cmmn, new_den // cmmn)
+
+    def __sub__(self, other_fraction):
+        new_den = lcd(self.den, other_fraction.den)
+        self_new_num = new_den // self.den * self.num
+        of_new_num = new_den // other_fraction.den * other_fraction.num
+        return Fraction(self_new_num - of_new_num, new_den)
+
+    def __gt__(self, other_fraction):
+        return self.num / self.den > other_fraction.num / other_fraction.den
+
+    def __lt__(self, other_fraction):
+        return not self.__gt__(other_fraction)
+
+
+# To make sure you understand how operators are implemented in Python classes, and how to properly write methods, write some methods to implement *, /, and - . Also implement comparison operators > and <
+
+x = Fraction(1, 2)
+# x.show()
+y = Fraction(2, 3)
+# print(y)
+# print(x + y)
+# print(x == y)
+# print(x * y)
+# print(x / y)
+# print(y - x)
+# print(y > x)
+# print(y < x)
