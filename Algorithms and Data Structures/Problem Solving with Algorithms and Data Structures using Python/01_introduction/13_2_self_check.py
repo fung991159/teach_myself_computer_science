e1f9f0fd9ffc72b44182c21fafe3f9d48ba47437
Fung Yeung <yeung.kei.fung@gmail.com>
2024-11-20 10:02:10 +1100

    renamed and add 13_2 self check

---
+++ Algorithms and Data Structures/Problem Solving with Algorithms and Data Structures using Python/01_introduction/13_2_self_check.py
@@ -0,0 +1,169 @@
+class LogicGate:
+
+    def __init__(self, lbl):
+        self.name = lbl
+        self.output = None
+
+    def get_label(self):
+        return self.name
+
+    def get_output(self):
+        self.output = self.perform_gate_logic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self, lbl):
+        super(BinaryGate, self).__init__(lbl)
+
+        self.pin_a = None
+        self.pin_b = None
+
+    def get_pin_a(self):
+        if self.pin_a == None:
+            return int(input("Enter pin A input for gate " + self.get_label() + ": "))
+        else:
+            return self.pin_a.get_from().get_output()
+
+    def get_pin_b(self):
+        if self.pin_b == None:
+            return int(input("Enter pin B input for gate " + self.get_label() + ": "))
+        else:
+            return self.pin_b.get_from().get_output()
+
+    def set_next_pin(self, source):
+        if self.pin_a == None:
+            self.pin_a = source
+        else:
+            if self.pin_b == None:
+                self.pin_b = source
+            else:
+                print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self, lbl):
+        BinaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+class OrGate(BinaryGate):
+
+    def __init__(self, lbl):
+        BinaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, lbl):
+        LogicGate.__init__(self, lbl)
+
+        self.pin = None
+
+    def get_pin(self):
+        if self.pin == None:
+            return int(input("Enter pin input for gate " + self.get_label() + ": "))
+        else:
+            return self.pin.get_from().get_output()
+
+    def set_next_pin(self, source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class NotGate(UnaryGate):
+
+    def __init__(self, lbl):
+        UnaryGate.__init__(self, lbl)
+
+    def perform_gate_logic(self):
+        if self.get_pin():
+            return 0
+        else:
+            return 1
+
+
+# Create a two new gate classes, one called NorGate the other called NandGate. NandGates work like AndGates that have a Not attached to the output. NorGates work lake OrGates that have a Not attached to the output.
+class NandGate(AndGate):
+
+    def perform_gate_logic(self):
+        if super().perform_gate_logic() == 1:
+            return 0
+        return 1
+
+
+class NorGate(OrGate):
+
+    def perform_gate_logic(self):
+        if super().perform_gate_logic() == 1:
+            return 0
+        return 1
+
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.from_gate = fgate
+        self.to_gate = tgate
+
+        tgate.set_next_pin(self)
+
+    def get_from(self):
+        return self.from_gate
+
+    def get_to(self):
+        return self.to_gate
+
+
+def main():
+    g1 = AndGate("G1")
+    g2 = AndGate("G2")
+    g3 = OrGate("G3")
+    g4 = NotGate("G4")
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
+    print(g4.get_output())
+
+
+# main()
+
+# Create a series of gates that prove the following equality NOT (( A and B) or (C and D)) is that same as NOT( A and B ) and NOT (C and D). Make sure to use some of your new gates in the simulation.
+g1 = AndGate("G1")
+g2 = AndGate("G2")
+g3 = NorGate("G3")
+c1 = Connector(g1, g3)
+c2 = Connector(g2, g3)
+
+g4 = AndGate("G4")
+g5 = AndGate("G5")
+g6 = NandGate("G6")
+c3 = Connector(g4, g6)
+c4 = Connector(g5, g6)
+
+
+# print(g5.get_output())
+# print(g3.get_output())
+# print(g6.get_output())
+print(g3.get_output() == g6.get_output())
